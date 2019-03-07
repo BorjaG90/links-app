4fa47ba48684b014a1414492ff8a050e10682afa
BorjaG90 <borjagete90@outlook.es>
2019-03-07 12:55:49 +0100

    Add Styles

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,4 +2,10 @@ const router = require('express').Router();
 
 const db = require('../database');
 
+router.get('/add', (req, res) => {
+  res.render('links/add');
+});
+
+router.post('/add');
+
 module.exports = router;
