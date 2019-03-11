676ad114f91729a2265c0966c8e65241faa7c14f
BorjaG90 <borjagete90@outlook.es>
2019-03-11 09:53:18 +0100

    Add encryption for password

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,7 +1,8 @@
 const passport  = require('passport'),
       Strategy  = require('passport-local').Strategy;
 
-const db = require('../database');
+const db        = require('../database'),
+      helpers   = require('../lib/helpers');
 
 passport.use('local.signup', new Strategy({
   usernameField: 'username',
@@ -14,5 +15,14 @@ passport.use('local.signup', new Strategy({
     password,
     fullname
   };
-  await db.query('INSERT INTO users SET ?', [newUser]);
+  newUser.password = await helpers.encryptPassword(password);
+
+  const result = await db.query('INSERT INTO users SET ?', [newUser]);
+  newUser.id = result.insertId;
+  
+  return done(null, newUser);
 }));
+
+passport.serializeUser((user, done) => {
+
+})
