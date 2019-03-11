aa9701674b53289ddd84cc0696eb4feb5bf562bb
BorjaG90 <borjagete90@outlook.es>
2019-03-11 12:00:16 +0100

    Add SignIn form

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -14,12 +14,12 @@ passport.use('local.signin', new LocalStrategy({
     const user = rows[0];
     const validPassword = await helpers.matchPassword(password, user.passport);
     if(validPassword){
-      done(null, user, req.flash('Welcome ' + user.username ));
+      done(null, user, req.flash('success', 'Welcome ' + user.username ));
     }else{
-      done(null, false, req.flash('Incorrect Password'));
+      done(null, false, req.flash('message', 'Incorrect Password'));
     }
   }else{
-    return done(null, false, req.flash('The Username does not exists'));
+    return done(null, false, req.flash('message', 'The Username does not exists'));
   }
 }));
 
