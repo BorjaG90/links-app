94835aba2921ea190a80aa7021d6a1f5a650aa58
BorjaG90 <borjagete90@outlook.es>
2019-03-11 09:32:10 +0100

    Add authentication logic

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,10 +1,18 @@
 const passport  = require('passport'),
       Strategy  = require('passport-local').Strategy;
 
+const db = require('../database');
+
 passport.use('local.signup', new Strategy({
   usernameField: 'username',
   passwordField: 'password',
   passReqToCallback: true //Recibir más campos
 }, async (req, username, password, done)=>{ //CallBack
-  done();
+  const { fullname } = req.body;
+  const newUser = {
+    username, //username: username
+    password,
+    fullname
+  };
+  await db.query('INSERT INTO users SET ?', [newUser]);
 }));
